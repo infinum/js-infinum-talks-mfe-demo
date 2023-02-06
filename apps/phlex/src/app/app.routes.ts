2973b061c383462f37e912697084b9e1ff24cf1b
Filip Voska <filip.voska@infinum.com>
2023-02-06 17:07:42 +0100

    add welcome component

--- apps/phlex/src/app/app.routes.ts
+++ apps/phlex/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { loadRemoteModule } from '@nrwl/angular/mf';
 
 export const appRoutes: Route[] = [
   {
+    path: '',
+    loadComponent: () => import('./components/welcome/welcome.component'),
+  },
+  {
     path: 'shows',
     loadChildren: () =>
       loadRemoteModule('shows', './Routes').then((m) => m.remoteRoutes),
---
+++ apps/phlex/src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'phlex-welcome',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
+})
+export default class WelcomeComponent {}
