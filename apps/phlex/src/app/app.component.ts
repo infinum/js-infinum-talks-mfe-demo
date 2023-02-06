301d870d2fe21baa5555ba3b2fec81717a40d1cc
Filip Voska <filip.voska@infinum.com>
2023-02-06 17:00:31 +0100

    add nicer navigation

--- apps/phlex/src/app/app.component.ts
+++ apps/phlex/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule],
   selector: 'phlex-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
